41491ddde93fb1510810ce5ce902c668b0673b1c
James Rumbal <J.Rumbal@windowslive.com>
2016-09-22 01:40:03 +1200

    Extend documentation, delete method on URLs work

--- flask_app.py
+++ flask_app.py
@@ -20,15 +20,14 @@ class LineInterface(Resource):
         """
         /line GET request will return lines in JSON format back to the user
 
-        Filters (Optional):
-            - line_id=<int> : filters results by id given
-            - name=<string> : filters results by searching for substring
+        Args:
+            - line_id=<int> : filters results by id given (Optional)
+            - name=<string> : filters results by searching for substring (Optional)
 
-        Returned:
-            - JSONObject: {'result': [line...]}
-            - Line Object: {'id': <int>,
-                            'name': <string>}
-
+        Returns:
+            - JSONObject: {"result": [Line...]}
+            - Line Object: {"id": <int>,
+                            "name": <string>}
         """
         args = request.args
         result = sess.query(Line)
@@ -40,11 +39,12 @@ class LineInterface(Resource):
         """
         /line PUT request will write or edit line objects into the database
 
-        Args:
-            - JSONArray: [line...]
-            - Line Object: {'id': <int>, (Optional: if given, overrides set in database. If excluded, creates new line)
-                            'name': <string>,
-                            'password': <string>}
+        Content-type: application/json
+        Payload:
+            - JSONArray: [Line...]
+            - Line Object: {"id": <int>, (Optional: if given, overrides set in database. If excluded, creates new line)
+                            "name": <string>,
+                            "password": <string>}
         """
 
         json_data = request.get_json()
@@ -78,16 +78,23 @@ class LineInterface(Resource):
 
     def delete(self):
         """
-        /line DELETE request will delete lines in the database
+        /line DELETE request will delete only 1 line in the database
+
+        Content-type: application/json
+        Payload:
+            - JSONObject: {"line_id": <int>,
+                           "password": <string>}
 
         """
-        args = request.args
-        if authenticate(args['number'], args['password']):
-            sess.query(Line).filter_by(id = args['number']).delete()
+        json_data = request.get_json()
+
+        if authenticate(json_data['line_id'], json_data['password']):
+            num = sess.query(Line).filter_by(id = json_data['line_id']).delete()
+            print(num)
             sess.commit()
             return None, 201
         else:
-            return None, 401
+            return {"message": "line doesn't exist"}, 401
 
 
 class TrapInterface(Resource):
@@ -95,12 +102,12 @@ class TrapInterface(Resource):
         """
         /trap GET request will return traps in JSON format back to the user
 
-        Filters (Optional):
+        Args (Optional):
         - line_id=<int> : filters results by line id given
         - trap_id=<int> : filters results by trap id given
 
-        Returned:
-        - JSONObject: {'result': [trap...]}
+        Returns:
+        - JSONObject: {'result': [Trap...]}
         - Trap Object: {'id': <int>,
                         'line_id': <int>,
                         'rebait_time': <long int>,
@@ -121,44 +128,46 @@ class TrapInterface(Resource):
         """
         /trap PUT request will write or edit trap objects into the database
 
-        Input:
-        - JSONArray: [trap...]
-        - Trap Object: {'id': <int>, (Optional: if given, overrides set in database. If excluded, creates new line)
-                        'line_id': <int>,
-                        'rebait_time': <long int>,
-                        'lat': <long int>,
-                        'long': <long int>,
-                        'line_order': <int>
-                        'broken': <boolean> (Optional on editing set, don't include if creating new trap)
-                        'moved': <boolean> (Optional on editing set, don't include if creating new trap)}
+        Content-type: application/json
+        Payload:
+            - JSONObject: {"line_id": <int>
+                           "password": <string>,
+                           "traps": [Trap...]}
+            - Trap Object: {'id': <int>, (Optional: if given, overrides set in database. If excluded, creates new line)
+                            'line_id': <int>,
+                            'rebait_time': <long int>,
+                            'lat': <long int>,
+                            'long': <long int>,
+                            'line_order': <int>
+                            'broken': <boolean> (Optional on editing set, don't include if creating new trap)
+                            'moved': <boolean> (Optional on editing set, don't include if creating new trap)}
         """
 
         json_data = request.get_json()
 
-        #authenticate(line_id, args['password']) # TODO: Return error if can't validate
+        if not authenticate(json_data['line_id'], json_data['password']):
+            return {"message": "could not validate password"}, 403
 
-        if not isinstance(json_data, collections.Iterable):
-            return {"error": {
-                        "code": 0, # TODO: Change error code
-                        "message": "Non iterable type returned"}}
+        if not isinstance(json_data['traps'], collections.Iterable):
+            return {"message": "non iterable datatype passed with traps"}, 400
 
         traps = []
-        for trap_data in json_data:
+        for trap_data in json_data['traps']:
             if "id" in trap_data: # ID passed, edit parameters of trap
                 trap = sess.query(Trap).filter_by(id=trap_data['id']).first()
 
-                #Edit values apart of trap
+                # Edit values apart of trap
                 trap.lat = trap_data['lat']
                 trap.long = trap_data['long']
-                trap.line_id = trap_data['line_id'] # Don't know if this should be here
-                trap.line_order = trap_data['line_order'] # Don't know if this should be here
-                trap.path_side = trap_data['path_side'] # Don't know if this should be here
+                trap.line_id = trap_data['line_id']  # Don't know if this should be here
+                trap.line_order = trap_data['line_order']  # Don't know if this should be here
+                trap.path_side = trap_data['path_side']  # Don't know if this should be here
                 if "broken" in trap_data:
                     trap.broken = trap_data['broken']
                 if "moved" in trap_data:
                     trap.moved = trap_data['moved']
 
-            else: # Trap doesn't exist, create a new trap
+            else:  # Trap doesn't exist, create a new trap
                 trap = Trap(datetime.date.fromtimestamp(trap_data['rebait_time']),
                             trap_data['lat'],
                             trap_data['long'],
@@ -170,18 +179,33 @@ class TrapInterface(Resource):
                 sess.add(trap)
 
         sess.commit()
-        return {'result': [trap.getDict() for trap in traps]}, 201
+        return {'result': [trap.id for trap in traps]}, 201
 
     def delete(self):
-        args = request.args
-        if authenticate(args['line_number'], args['password']):
-            trap = sess.query(Trap).filter_by(id = args['trap_number']).first()
-            if trap.line_id == args['line_number']:
-                sess.query(Trap.filter_by(id = args['trap_number'])).delete()
-                sess.commit()
+        """
+        /trap DELETE request will delete multiple traps in the database belonging to one line
+
+        Content-type: application/json
+        Payload:
+            - JSONObject: {"line_number": <int>,
+                           "password": <string>,
+                           "traps": [<int>...]}
+
+        """
+        json_data = request.get_json()
+        if authenticate(json_data['line_number'], json_data['password']):
+            # Make sure they all belong to the same line
+            traps = sess.query(Trap).filter(Trap.id.in_(json_data['traps'])).all()
+            for trap in traps:
+                if trap.line_id != json_data['line_number']:
+                    return {"message": "trap belongs to different line"}, 400
+
+            # Delete traps
+            sess.query(Trap).filter(Trap.id.in_(json_data['traps'])).delete(synchronize_session='fetch')
+            sess.commit()
             return None, 201
         else:
-            return None, 401
+            return {"message": "could not validate user"}, 401
 
 
 class CatchInterface(Resource):
@@ -189,46 +213,49 @@ class CatchInterface(Resource):
         """
         /catch GET request will return catches in JSON format back to the user
 
-        Filters (Optional):
-        - line_id=<int> : filters results by line id given by relationship query to a trap
-        - trap_id=<int> : filters results by trap id given
+        Args:
+        - line_id=<int> : filters results by line id given by relationship query to a trap (Optional)
+        - trap_id=<int> : filters results by trap id given (Optional)
 
         Returned:
-        - JSONObject: {'result': [catch...]}
-        - Catch Object: {'id': <int>,
-                        'trap_id: <int>,
-                        'animal_id': <int>,
-                        'time': <long int>}
+            JSONObject: {'result': [catch...]}
+            Catch Object: {'id': <int>,
+                           'trap_id: <int>,
+                           'animal_id': <int>,
+                           'time': <long int>}
         """
         args = request.args
         result = sess.query(Catch)
-        if 'line_id' in args: result = result.join(Trap.id).filter(Trap.line_id == args['line_id']) # TODO: Test relationship query
+        print(args['line_id'])
+        if 'line_id' in args: result = sess.query(Catch).join(Trap).filter(Trap.line_id == args['line_id'])
         if 'trap_id' in args: result = result.filter_by(id=args['trap_id'])
 
-        return {'result': [catch.getDict() for catch in result.all()]}, 200
+        return {'result': [catch.id for catch in result.all()]}, 200
 
     def put(self):
         """
         /catch PUT request will write or edit catch objects into the database
 
-        Input:
-        - JSONArray: [catch...]
-        - Catch Object: {'id': <int>, (Optional: if given, overrides set in database. If excluded, creates new line)
-                        'trap_id': <int>,
-                        'animal_id': <int>,
-                        'time': <long int> (Ignored when overriding set in database)}
+        Content-type: application/json
+        Payload:
+            JSONObject:   {"line_id": <int>
+                           "password": <string>,
+                           "catches": [Catch...]}
+            Catch Object: {'id': <int>, (Optional: if given, overrides set in database. If excluded, creates new line)
+                           'trap_id': <int>,
+                           'animal_id': <int>,
+                           'time': <long int> (Ignored when overriding set in database)}
         """
         json_data = request.get_json()
 
-        #authenticate(line_id, args['password']) # TODO: Return error if can't validate
+        if not authenticate(json_data['line_id'], json_data['password']):
+            return {"message": "could not validate password"}, 403
 
-        if not isinstance(json_data, collections.Iterable):
-            return {"error": {
-                        "code": 0, # TODO: Change error code
-                        "message": "Non iterable type returned"}}
+        if not isinstance(json_data["catches"], collections.Iterable):
+            return {"message": "non iterable datatype passed with traps"}
 
         catches = []
-        for catch_data in json_data:
+        for catch_data in json_data["catches"]:
             if "id" in catch_data:
                 catch = sess.query(Catch).filter_by(id=catch_data['id']).first()
 
@@ -247,19 +274,23 @@ class CatchInterface(Resource):
         sess.commit()
         return {'result': [catch.getDict() for catch in catches]}, 201
 
-    def delete(self):
-        pass
-
 
 def authenticate(line_id, password):
     """
     Authenticate an inputed password by comparing the line_id hashed password against given password
 
-    Arguments:
-    line_id -- Line id to compared hashed password stored in database to
-    password -- Password to compared hashed password against
+    Args:
+        line_id -- Line id to compared hashed password stored in database to
+        password -- Password to compared hashed password against
     """
+    print(line_id)
+    print(password)
+
+
     line = sess.query(Line).filter_by(id=line_id).first()
+    if line is None:
+        return False
+
     hash_compare = hashlib.pbkdf2_hmac('sha1', str.encode(password), binascii.unhexlify(line.salt), 100000)
 
     return binascii.hexlify(hash_compare).decode("utf-8") == line.password_hashed
