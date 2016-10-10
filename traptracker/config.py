ff2fae640e963dcbc36d7bf3ace3cba0a50ace5b
Cameron Dykstra <dykstra.cameron@gmail.com>
2016-10-10 00:40:05 +0000

    added editing/adding traps on website, closes #19

--- traptracker/config.py
+++ traptracker/config.py
@@ -2,3 +2,6 @@ from traptracker.default_config import *
 
 # Override settings in default_config here
 # Don't commit local changes to this file to version control
+
+RECAPTCHA_PUBLIC_KEY = '6LeJzAgUAAAAAGKWf3YutPWdB-a9NmEcb2ysEvTH'
+RECAPTCHA_PRIVATE_KEY = '6LeJzAgUAAAAAJhSViZEtbHzH7QpxF4iNqt2w2-d'
